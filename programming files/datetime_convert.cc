2a6e312b944808ff5d737800a634d9fcd6e252c2
Darby Lane <166237578+dlane13@users.noreply.github.com>
2025-04-01 16:44:06 -0400

    Update datetime_convert.cc
    i added a comment!

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -8,7 +8,7 @@ int main()
     std::string date_str = "2022-03-17 10:45:30";
     std::tm date_obj = {};
     std::istringstream ss(date_str);
-    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S"); //comment
     std::stringstream formatted_date_ss;
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
